e50e1f0bb4e68ed7fbe0c522e159176516a6295f
tylerjbrennan <tylerjbrennan@gmail.com>
2019-07-25 12:19:02 -0400

    Update game.js

--- source/game.js
+++ source/game.js
@@ -5,6 +5,37 @@ var memory_array_numbers = ["One", "Uno", "Two", "Dos", "Three", "Tres", "Four",
 var memory_values = [];
 var memory_tile_ids = [];
 var tiles_flipped = 0;
+
+var h1 = document.getElementsByTagName('h3')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+timer();
+function stop() {
+    clearTimeout(t);
+}
+function clear() {
+    h1.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+}
 Array.prototype.memory_tile_shuffle = function(){
     var i = this.length, j, temp;
     while(--i > 0){
